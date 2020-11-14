59fe5a7bc2fd0996243aa40a24569a713174a13b
proletarius101 <proletarius101@protonmail.com>
2020-11-14 17:54:31 +0800

    remove analytics, gms, firebase

--- shared/src/main/java/com/oasisfeng/island/analytics/AnalyticsImpl.kt
+++ shared/src/main/java/com/oasisfeng/island/analytics/AnalyticsImpl.kt
@@ -2,15 +2,7 @@ package com.oasisfeng.island.analytics
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.annotation.CheckResult
-import com.google.android.gms.analytics.GoogleAnalytics
-import com.google.android.gms.analytics.HitBuilders.EventBuilder
-import com.google.android.gms.analytics.Tracker
-import com.google.firebase.analytics.FirebaseAnalytics
-import com.oasisfeng.island.firebase.FirebaseWrapper
-import com.oasisfeng.island.shared.BuildConfig
-import com.oasisfeng.island.shared.R
 import org.intellij.lang.annotations.Pattern
 
 /**
@@ -34,38 +26,8 @@ internal class AnalyticsImpl(context: Context) : Analytics {
 	override fun report(t: Throwable) = CrashReport.logException(t)
 	override fun report(message: String, t: Throwable) { CrashReport.log(message); CrashReport.logException(t) }
 
-	override fun reportEvent(event: String, params: Bundle) {
-		Log.d(TAG, if (params.isEmpty) "Event: $event" else "Event: $event $params")
-		val builder = EventBuilder().setCategory(event)
-		val category = params.getString(Analytics.Param.ITEM_CATEGORY.key)
-		val id = params.getString(Analytics.Param.ITEM_ID.key)
-		val name = params.getString(Analytics.Param.ITEM_NAME.key)
-		if (category != null) {
-			builder.setAction(category)
-			require(! BuildConfig.DEBUG || name == null) { "Category and Name cannot be used at the same time: $event" }
-		} else if (name != null) builder.setAction(name)
-		if (id != null) builder.setLabel(id)
-		mGoogleAnalytics.send(builder.build())
-		mFirebaseAnalytics.logEvent(event, params)
-		CrashReport.log("Event: $event ${params.toString().substring(6/* Remove leading "Bundle" */)}")
-	}
-
-	override fun setProperty(property: Analytics.Property, value: String) {
-		mGoogleAnalytics["&cd" + property.ordinal + 1] = value // Custom dimension (index >= 1)
-		mFirebaseAnalytics.setUserProperty(property.key, value)
-		CrashReport.setProperty(property.key, value)
-	}
-
-	private val mGoogleAnalytics: Tracker
-	private val mFirebaseAnalytics: FirebaseAnalytics
-
-	init {  // TODO: De-dup the user identity between Mainland and Island.
-		val googleAnalytics = GoogleAnalytics.getInstance(context)
-		if (BuildConfig.DEBUG) googleAnalytics.setDryRun(true)
-		mGoogleAnalytics = googleAnalytics.newTracker(R.xml.analytics_tracker)
-		mGoogleAnalytics.enableAdvertisingIdCollection(true)
-		mFirebaseAnalytics = FirebaseAnalytics.getInstance(FirebaseWrapper.init())
-	}
+	override fun reportEvent(event: String, params: Bundle) { }
+	override fun setProperty(property: Analytics.Property, value: String) {	}
 }
 
 private const val TAG = "Analytics"
